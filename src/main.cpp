ebf1e086de773d5d207cc8c6c96819eb99fb5ef4
Eyjafja <arkhipovfed@gmail.com>
2022-08-01 11:50:42 +0300

    keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main()
     std::array<std::array<double, 3>, 3> DirMat = {{{1, 0, 0},//Прямая Матрица
                                                     {0, 1, 0},
                                                     {0, 0, 1}}};
+    bool up = false, down = false, left = false, right = false;
     std::vector<Dot> DotVec;
     for(int i = 0; i < Number_Of_dots; i++){//Создание точек
         Dot dot{};
@@ -26,10 +27,31 @@ int main()
         window.clear(sf::Color::White);
         window.draw(Space);
 
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) DirMat = MultMatrix(DirMat, XSpin); //Повороты сферы
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) DirMat = MultMatrix(DirMat, YSpin);
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) DirMat = MultMatrix(DirMat, NegativeXSpin);
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) DirMat = MultMatrix(DirMat, NegativeYSpin);
+        if (event.type == sf::Event::KeyPressed) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) up = true;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) down = true;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) left = true;
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) right = true;
+        }
+        else if (event.type == sf::Event::KeyReleased) {
+            if (event.key.code == sf::Keyboard::W) up = false;
+            if (event.key.code == sf::Keyboard::S) down = false;
+            if (event.key.code == sf::Keyboard::A) left = false;
+            if (event.key.code == sf::Keyboard::D) right = false;
+        }
+
+        if(up){
+                DirMat = MultMatrix(DirMat, XSpin);
+        }
+        if(down){
+            DirMat = MultMatrix(DirMat, NegativeXSpin);
+        }
+        if(left){
+            DirMat = MultMatrix(DirMat, YSpin);
+        }
+        if(right){
+            DirMat = MultMatrix(DirMat, NegativeYSpin);
+        }
 
         for(int i = 0; i < Number_Of_dots; i++) DotVec[i].BackDraw(DirMat); //Отрисовка точек на сфере
         for(int i = 0; i < Number_Of_dots; i++) DotVec[i].FrontDraw(DirMat);
